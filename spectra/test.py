f84ec859c8e9805501d7f5a3a59ff5183d4d723f
g.baptista <g.baptista@campus.fct.unl.pt>
2023-07-10 18:51:01 +0100

    commit before dev branch

--- spectra/test.py
+++ spectra/test.py
@@ -1,24 +1,63 @@
 import pandas as pd
 import numpy as np
 from scipy.special import voigt_profile
+import scienceplots
 import matplotlib.pyplot as plt
+plt.style.use(['science','nature'])
 
 def plot_spectra(orbitals):
-    fig,ax=plt.subplots(1,1)
-    en_x=np.linspace(8000,8100,10000)
+
+    en_x=np.linspace(8000,8080,10000)
+    fig_all,ax_all=plt.subplots(2,1,figsize=(8,12),sharex=True,sharey=False)
+    fig_all.subplots_adjust(hspace=0)
+    fig_all.tight_layout()
+    colors = plt.cm.tab20c.colors
+    ccount=0
     for i in orbitals:
-        df_diag=pd.read_csv('spectra/Cu_'+i+'_spectrum_diagram.csv')
+        fig,ax=plt.subplots(3,1,figsize=(8,12),sharex=True,sharey=False)
+        fig.subplots_adjust(hspace=0)
+        fig.tight_layout()
+        df_diag=pd.read_csv('spectra/data/Cu_'+i+'_spectrum_diagram.csv')
         df_diag=df_diag[['Energy (eV)','Intensity (a.u.)','Energy width (eV)','Initial Config Label','Final Config Label','Final Config 2jj','Final Config eig']].values
         intens_y=0*en_x
+        max_tot=0
         for j in df_diag:
             intens_y+=j[1]*voigt_profile(en_x-j[0],0,j[2])
-            if j[0] > en_x[0] and j[0] < en_x[-1]:
-                ax.plot(en_x,j[1]*voigt_profile(en_x-j[0],0,j[2]))
-                print(j)
-        #ax.plot(en_x,intens_y,label=i)
             
-    ax.legend()
+            if j[0] > 8010 and j[0] < 8080:
+                ax[1].plot(en_x,j[1]*voigt_profile(en_x-j[0],0,j[2]))
+                max_y=(j[1]*voigt_profile(en_x-j[0],0,j[2])).max()
+                if max_y>max_tot:max_tot=max_y
+                ax[2].vlines(j[0],ymin=0,ymax=j[1])
+                #ax[1].set_yscale('log')
+                #print(j)
+        ax[0].plot(en_x,intens_y/intens_y.max(),label=i)
+        #fig.suptitle(r'4p spectator electron $K\alpha$ emission spectrum')
+        #ax[1].set_ylim(0,max_tot)
+        ax[2].set_xlabel('Energy (eV)',fontsize=8)
+        ax[1].set_ylabel('Intensity (a.u)',fontsize=8)
+        ax[0].set_ylabel('Normalized Intensity (a.u)',fontsize=8)
+        ax[2].set_ylabel('Intensity (a.u)',fontsize=8)
+
+        ax[0].text(0.05, 0.9, "Full spectrum", transform=ax[0].transAxes, fontsize=8, va='top', ha='left')
+        ax[0].text(0.9, 0.9, f"{i} spectator electron", transform=ax[0].transAxes, fontsize=12, va='top', ha='right')
+        ax[1].text(0.05, 0.9, "Components spectrum", transform=ax[1].transAxes, fontsize=8, va='top', ha='left')
+        ax[2].text(0.05, 0.9, "Components' energy centroids", transform=ax[2].transAxes, fontsize=8, va='top', ha='left')
+        for k in ax:
+            k.tick_params(axis='both', which='major', labelsize=8)
+        fig.savefig(f'spectra/spec_figs/{i}.pdf',dpi=300)
+        ax_all[0].plot(en_x,intens_y,label=i,color=colors[ccount % len(colors)])
+        ax_all[1].plot(en_x,intens_y/intens_y.max(),label=i,color=colors[ccount % len(colors)])
+        ccount+=1
+    ax_all[0].legend( prop={'size': 8})
+    ax_all[1].legend( prop={'size': 8})
+    for k in ax_all:
+            k.tick_params(axis='both', which='major', labelsize=8)
+    ax_all[1].set_xlabel('Energy (eV)',fontsize=8)
+    ax_all[0].set_ylabel('Intensity (a.u)',fontsize=8)
+    ax_all[1].set_ylabel('Normalized Intensity (a.u)',fontsize=8)
+    #fig_all.savefig(f'spectra/spec_figs/all.pdf',dpi=300)
 
 
-plot_spectra(['4p'])
-plt.savefig('spectra/test.pdf',dpi=300)
+plot_spectra(['7p'])
+plt.show()
--- spectra/to_convert/convert_to_csv.py
+++ spectra/to_convert/convert_to_csv.py
@@ -1,7 +1,11 @@
 import pandas as pd
-orbital='5p'
-df = pd.read_csv(f'spectra/to_convert/Cu_{orbital}_rates_rad.txt',sep='\t')
-
+orbital='5f'
+df = pd.read_csv(f'spectra/to_convert/Cu_{orbital}_rates_rad.csv')[['Shell IS ', ' IS 2JJ ', ' IS eigenvalue ', ' Shell FS ', ' FS 2JJ ',
+       ' FS eigenvalue ', ' transition energy [eV] ', ' rate [s-1] ']]
+new_names={'Shell IS ':'Initial Config Label', ' IS 2JJ ':'Initial Config 2jj', ' IS eigenvalue ':'Initial Config eig', ' Shell FS ':'Final Config Label', ' FS 2JJ ':'Final Config 2jj',
+       ' FS eigenvalue ':'Final Config eig', ' transition energy [eV] ':'Energy (eV)', ' rate [s-1] ':'Rate (s-1)'}
+df=df.rename(columns=new_names)
+df.to_csv(f'spectra/data/Cu_{orbital}_rates_rad.csv',index=False)
 
 
 # df=pd.read_csv(f'spectra/Cu_{orbital}_spectrum_diagram.txt',sep='\t')[[ ' Shell IS ', ' IS 2JJ ',
